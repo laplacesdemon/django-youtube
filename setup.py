ec0140b61e6dc8f96f39d01e8de6e51d81c551e3
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2012-09-08 21:03:20 +0300

    setup files are added

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-youtube',
+    version='0.1',
+    author=u'Suleyman Melikoglu',
+    author_email='suleyman@melikoglu.info',
+	packages=find_packages(),
+	include_package_data=True
+    url='https://github.com/laplacesdemon/django-youtube',
+    license='BSD licence, see LICENCE.txt',
+    description='Youtube API wrapper app for Django.' + \
+                ' It helps to upload, display, delete, update videos from Youtube',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+)
